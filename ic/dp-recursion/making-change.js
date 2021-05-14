347577029d0a25343e677a1347a1ac950fb809a6
jspark721 <jspark721@gmail.com>
2021-05-14 15:56:34 -0700

    initial commit

---
+++ ic/dp-recursion/making-change.js
@@ -0,0 +1,14 @@
+/* write a function that, given:
+
+1. an amount of money
+2. an array of coin denominations
+
+commutes the number of ways to make the amount of money with coins of the available denominations
+
+example: for amount=4 (4 cents) and denominations=[1,2,3] (1c,2c, 3 cents), your program would output 4 -- the number of ways to make 4 cents with those denominations: 
+1. 1c,1c,1c,1c
+2. 1c,1c,2c
+3. 1c,3c
+4. 2c,2c
+
+*/

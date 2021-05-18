808c47b15ce5dfe88eb85ca66dfbfdbc0f7f8035
Julie Park <jspark721@gmail.com>
2021-05-17 22:20:19 -0700

    Create spiral-matrix.js

---
+++ b2bswe/arrays/spiral-matrix.js
@@ -0,0 +1,27 @@
+/*
+Spiral Traversal of A Matrix
+
+Given a two-dimentional matrix with m rows and n columns (m x n matrix), return its spiral ordering starting from the top left of the matrix (row 0, col 0)
+
+Input:
+[
+ [ 1, 2, 3],
+ [ 4, 5, 6],
+ [ 7, 8, 9]
+]
+Output: [1,2,3,6,9,8,7,4,5]
+
+Input:
+[
+  [1,2],
+  [3,4],
+  [5,6],
+  [7,8]
+]
+Output: [1,2,4,6,8,7,5,3]
+
+edge case: empty array
+optimize
+time complexity:
+space complexity: 
+*/
